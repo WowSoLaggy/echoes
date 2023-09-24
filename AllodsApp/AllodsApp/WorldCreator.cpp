48d7ee782cfe7c5c80edcd0bf86be6ca43ea0e54
WowSoLaggy <dohxehapo@gmail.com>
2023-09-24 18:14:23 +0400

    (*) Updated default world

--- AllodsApp/AllodsApp/WorldCreator.cpp
+++ AllodsApp/AllodsApp/WorldCreator.cpp
@@ -21,7 +21,45 @@ std::unique_ptr<World> WorldCreator::createTest()
 
   //
 
-  createStr("Lattice", 8, 5);
+  for (int x = 5; x <= 11; ++x)
+  {
+    createStr("Lattice", x, 2);
+    createStr("Lattice", x, 8);
+  }
+
+  for (int y = 3; y <= 7; ++y)
+  {
+    createStr("Lattice", 5, y);
+    createStr("Lattice", 11, y);
+  }
+
+  for (int x = 6; x <= 10; ++x)
+  {
+    for (int y = 3; y <= 7; ++y)
+      createStr("Plating", x, y);
+  }
+
+  for (int x = 7; x <= 9; ++x)
+  {
+    for (int y = 4; y <= 6; ++y)
+      createStr("Floor", x, y);
+  }
+
+  for (int x = 6; x <= 10; ++x)
+  {
+    createStr("Wall", x, 3);
+    if (x != 8)
+      createStr("Wall", x, 7);
+  }
+
+  for (int y = 4; y <= 6; ++y)
+  {
+    createStr("Wall", 6, y);
+    createStr("Wall", 10, y);
+  }
+
+  createStr("Floor", 8, 7);
+  createStr("Door", 8, 7);
 
   return world;
 }
