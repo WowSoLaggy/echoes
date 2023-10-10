c45482ae6b72e25c552fcf9207d1bc0430ee50aa
Anton Egorov <dohxehapo@gmail.com>
2023-10-10 22:14:39 +0300

    (+) Added 'ObjectView'

--- AllodsApp/AllodsApp/ObjectView.cpp
+++ AllodsApp/AllodsApp/ObjectView.cpp
@@ -1,27 +1,29 @@
 #include "stdafx.h"
-#include "StructureView.h"
+#include "ObjectView.h"
 
 #include <LaggyDx/AnimatedSprite.h>
 #include <LaggyDx/ISpriteShader.h>
 
 
-StructureView::StructureView(const Dx::ISpriteShader& i_shader)
+ObjectView::ObjectView(const Dx::ISpriteShader& i_shader)
   : d_shader(i_shader)
 {
 }
 
 
-void StructureView::render(
-  const Dx::ITexture* i_texture, Sdk::Vector2I i_position,
+void ObjectView::render(
+  const Dx::ITexture* i_texture, const Sdk::Vector2I& i_position,
   int i_animationFrame, Dx::Color i_color) const
 {
   Dx::AnimatedSprite sprite;
 
   sprite.setTexture(i_texture);
   sprite.resetSizeToTexture();
-  sprite.setPosition(std::move(i_position));
-  sprite.setColor(std::move(i_color));
   sprite.setCurrentFrame(i_animationFrame);
+  sprite.setColor(std::move(i_color));
+
+  const auto pos = i_position - sprite.getSize() / 2;
+  sprite.setPosition(pos);
 
   d_shader.draw(sprite);
 }
--- AllodsApp/AllodsApp/ObjectsView.cpp
+++ AllodsApp/AllodsApp/ObjectsView.cpp
@@ -2,27 +2,17 @@
 #include "ObjectsView.h"
 
 #include "Object.h"
+#include "ObjectView.h"
 #include "Prototypes.h"
-
-#include <LaggyDx/AnimatedSprite.h>
-#include <LaggyDx/ISpriteShader.h>
 
 
 void ObjectsView::render(const Dx::ISpriteShader& i_shader, const Objects& i_objects) const
 {
-  Dx::AnimatedSprite sprite;
+  ObjectView objectView(i_shader);
 
   for (const auto objectPtr : i_objects)
   {
     const auto& object = SAFE_DEREF(objectPtr);
-
-    sprite.setTexture(object.getPrototype().texture);
-    sprite.setCurrentFrame(object.getAnimationPlayer().getCurrentFrame());
-    sprite.resetSizeToTexture();
-
-    const auto pos = object.getPosition() - object.getSize() / 2;
-    sprite.setPosition(pos);
-
-    i_shader.draw(sprite);
+    objectView.render(object.getPrototype().texture, object.getPosition(), object.getAnimationPlayer().getCurrentFrame());
   }
 }
